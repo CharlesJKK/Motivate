1d540495d74643707ebe895eb94e033b588edbde
CharlesJKK <selrahcjunior@hotmail.com>
2023-04-14 14:47:13 -0300

    feat: adicionando ação de click no botão e viewbinding

--- app/src/main/java/com/example/motivate/MainActivity.kt
+++ app/src/main/java/com/example/motivate/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.motivate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.motivate.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private  lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.buttonNewPhrase.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if(view.id == R.id.button_new_phrase){
+            var s = ""
+        }
     }
 }
